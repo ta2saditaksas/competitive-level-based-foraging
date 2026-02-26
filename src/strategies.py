6c7cd1fba35aadcdfaecc392b378fa830c0a657d
Amel <amel.ben.chabane22@gmail.com>
2026-02-26 16:07:51 +0100

    Strategie fictitious play

--- src/strategies.py
+++ src/strategies.py
@@ -10,3 +10,11 @@ def strategy_random_uniform(items):
     return [random.choice(fioles_dispos) for _ in range(len(items))]
 
 
+#strategie fictitious play (apprentissage à partir de l'historique de l'équipe 0) ---
+def strategy_fictitious_play(items, history_team_0):
+    """
+    l equipe 1 choisit les fioles bases sur les choix passes de equipe 0
+    """
+    fiole_cpt = {fiole: history_team_0.count(fiole) for fiole in items}
+    fiole_plus_choisie = max(fiole_cpt, key=fiole_cpt.get)
+    return [fiole_plus_choisie] * len(items) #ici tous les joueurs choisisent la meme fiole
