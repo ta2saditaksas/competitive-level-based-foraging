066cf983c203e6dd7d3ebfa98c14e8d06ffc9ef3
Amel <amel.ben.chabane22@gmail.com>
2026-03-18 11:18:40 +0100

    Strategie stationnaire tetue

--- src/strategies.py
+++ src/strategies.py
@@ -10,7 +10,7 @@ def strategy_random_uniform(items):
     return [random.choice(fioles_dispos) for _ in range(len(items))]
 
 
-#strategie fictitious play (apprentissage à partir de l'historique de l'équipe 0) ---
+#strategie fictitious play (apprentissage a partir de l historique de l equipe 0
 def strategy_fictitious_play(items, history_team_0):
     """
     l equipe 1 choisit les fioles bases sur les choix passes de equipe 0
@@ -18,3 +18,10 @@ def strategy_fictitious_play(items, history_team_0):
     fiole_cpt = {fiole: history_team_0.count(fiole) for fiole in items}
     fiole_plus_choisie = max(fiole_cpt, key=fiole_cpt.get)
     return [fiole_plus_choisie] * len(items) #ici tous les joueurs choisisent la meme fiole
+
+#strategie stationnaire tetue
+def strategy_stationary(tetue_allocation):
+    """
+    on attribue des fioles fixes pour chaque joueur
+    """
+    return tetue_allocation
